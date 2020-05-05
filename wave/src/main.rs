1aa4d765e44dd6912fff706966f4205580b3ecc8
Silas Berger <silas.berger@outlook.com>
2020-05-05 23:54:52 +0200

    Clean up main.rs and remove play.sh.

--- wave/src/main.rs
+++ wave/src/main.rs
@@ -7,11 +7,11 @@ use wave::output;
 const SAMPLE_RATE: u16 = 44100;
 
 fn main() {
-    the_licc_wav();
+    a_major_with_melody();
 }
 
 #[allow(dead_code)]
-fn the_licc_wav() {
+fn the_licc() {
     let g = pitch(Note::G, 4);
     let a = pitch(Note::A, 4);
     let b = pitch(Note::B, 4);
@@ -27,30 +27,7 @@ fn the_licc_wav() {
         FragmentSpec::note(1, g, 0.5),
         FragmentSpec::note(1, a, 0.5)
     ];
-    let wave = sine::bounce(&fragments, &track_spec);
-    let wav_encoded = output::encode_wav(wave, &track_spec);
-    write_to_file("the_licc.wav", &wav_encoded);
-}
-
-#[allow(dead_code)]
-fn the_licc() {
-    let g = pitch(Note::G, 4);
-    let a = pitch(Note::A, 4);
-    let b = pitch(Note::B, 4);
-    let c = pitch(Note::C, 5);
-    let d = pitch(Note::D, 5);
-    let track_spec = TrackSpec::new(SAMPLE_RATE, 60, 8);
-    let fragments = vec![
-        FragmentSpec::note(1, a, 0.5),
-        FragmentSpec::note(1, b, 0.5),
-        FragmentSpec::note(1, c, 0.5),
-        FragmentSpec::note(1, d, 0.5),
-        FragmentSpec::note(2, b, 0.5),
-        FragmentSpec::note(1, g, 0.5),
-        FragmentSpec::note(1, a, 0.5)
-    ];
-    let wave = sine::bounce(&fragments, &track_spec);
-    write_to_file("the_licc.pcm", &wave);
+    bounce_and_export("the_licc.wav", &fragments, &track_spec);
 }
 
 #[allow(dead_code)]
@@ -58,25 +35,20 @@ fn a_major_with_melody() {
     let a = pitch(Note::A, 4);
     let c_sharp = pitch(Note::CSharp, 5);
     let e = pitch(Note::E, 5);
+    let track_spec = TrackSpec::new(SAMPLE_RATE, 80, 4);
     let fragments = vec![
-        FragmentSpec::note(500, a, 0.5),
-        FragmentSpec::note(500, c_sharp, 0.5),
-        FragmentSpec::note(500, e, 0.3),
-        FragmentSpec::chord(1000, vec![a, c_sharp, e], 0.5)
+        FragmentSpec::note(1, a, 0.5),
+        FragmentSpec::note(1, c_sharp, 0.5),
+        FragmentSpec::note(1, e, 0.3),
+        FragmentSpec::chord(2, vec![a, c_sharp, e], 0.5)
     ];
-    let track_spec = TrackSpec::new(SAMPLE_RATE, 80, 8);
-    let wave = sine::bounce(&fragments, &track_spec);
-    write_to_file("melody_and_chord.pcm", &wave);
+    bounce_and_export("melody_and_chord.wav", &fragments, &track_spec);
 }
 
-#[allow(dead_code)]
-fn a_major() {
-    let fragment = FragmentSpec::chord(1000,
-                                       vec![440.0, 554.37, 659.25],
-                                       0.8);
-    let track_spec = TrackSpec::new(SAMPLE_RATE, 80, 8);
-    let wave = sine::bounce(&vec![fragment], &track_spec);
-    write_to_file("new_power_chord.pcm", &wave);
+fn bounce_and_export(filename: &str, fragments: &[FragmentSpec], track_spec: &TrackSpec) {
+    let data = sine::bounce(fragments, track_spec);
+    let wav_encoded = output::encode_wav(data, &track_spec);
+    write_to_file(filename, &wav_encoded);
 }
 
 fn write_to_file(filename: &str, buf: &[u8]) {
